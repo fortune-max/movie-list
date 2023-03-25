204b312b24cbace2fca444d64d711f93a8378add
fortune-max <fortmax120@gmail.com>
2023-03-25 16:31:00 +0100

    add client, public

---
+++ app/libs/client.ts
@@ -0,0 +1,12 @@
+import { GraphQLClient } from 'graphql-request'
+import { getSdk } from '~/generated/graphql'
+
+export const GRAPHQL_ENDPOINT = 'https://harbour-movies.vercel.app/api/graphql';
+
+const client = new GraphQLClient(GRAPHQL_ENDPOINT, {
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+export const sdk = getSdk(client)
